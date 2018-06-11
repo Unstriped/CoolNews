6ebc16aeab3acdc3c1c38876cb6870ccad462224
Stefan Magnusson <stefanmagnusson90@gmail.com>
2018-06-11 09:03:01 +0200

    Updated UI

--- src/main/java/com/example/stefan/coolnews/MainActivity.java
+++ src/main/java/com/example/stefan/coolnews/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.stefan.coolnews;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,16 +28,35 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private Context mContext;
+    private RecyclerView mRecyclerView;
+    private SwipeRefreshLayout mSwipeLayout;
+    private TextView mFeedPubDateTextView;
+    private TextView mFeedTitleTextView;
+    private TextView mFeedLinkTextView;
+    private TextView mFeedDescriptionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.RED));
+        mContext = getApplicationContext();
+
+        //mRecyclerView = findViewById(R.id.recyclerView);
+        //mSwipeLayout = findViewById(R.id.swipeRefreshLayout);
+       /* mFeedPubDateTextView = findViewById(R.id.newsPubDate);
+        mFeedTitleTextView = findViewById(R.id.newsTitle);
+        mFeedDescriptionTextView = findViewById(R.id.newsDescription);
+        mFeedLinkTextView = findViewById(R.id.newsLink);*/
+
         FetchNews();
     }
 
     public void FetchNews(){
+
+
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
@@ -38,17 +66,55 @@ public class MainActivity extends AppCompatActivity {
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                LinearLayout myRoot = (LinearLayout) findViewById(R.id.my_root);
+                LinearLayout a = new LinearLayout(mContext);
+                a.setOrientation(LinearLayout.VERTICAL);
+
                 Log.v(TAG, "Fetching News...");
                 for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
                     NewsContent nContent = singleSnapshot.getValue(NewsContent.class);
+                    String date  = nContent.getNewsPubDate();
+                    String title  = nContent.getNewsTitle();
+                    String description  = nContent.getNewsDescription();
+                    String link  = nContent.getNewsLink();
 
-                    Log.v(TAG, "------------------------------------------------------------------------------------");
-                    Log.v(TAG, nContent.getNewsPubDate());
-                    Log.v(TAG, nContent.getNewsTitle());
-                    Log.v(TAG, nContent.getNewsDescription());
-                    Log.v(TAG, nContent.getNewsLink());
-                    Log.v(TAG, "------------------------------------------------------------------------------------");
+                    // temp fix to hide empty news
+                    if(description == null){
+                        continue;
+                    }
+
+
+                    TextView dateView = new TextView(mContext);
+                    dateView.setText(date);
+                    dateView.setTypeface(Typeface.MONOSPACE);
+
+                    TextView titleView = new TextView(mContext);
+                    titleView.setText(title);
+                    titleView.setTypeface(Typeface.DEFAULT_BOLD);
+
+                    TextView descView = new TextView(mContext);
+                    descView.setText(description);
+
+                    TextView linkView = new TextView(mContext);
+                    linkView.setText(link);
+                    linkView.setPadding(0,0,0,30);
+                    linkView.setTypeface(Typeface.defaultFromStyle(Typeface.ITALIC));
+
+                    a.addView(dateView);
+                    a.addView(titleView);
+                    a.addView(descView);
+                    a.addView(linkView);
+
+
+
+                   /* mFeedPubDateTextView.setText(date);
+                    mFeedTitleTextView.setText(title);
+                    mFeedDescriptionTextView.setText(description);
+                    mFeedLinkTextView.setText(link);*/
                 }
+
+                myRoot.addView(a);
             }
 
             @Override
